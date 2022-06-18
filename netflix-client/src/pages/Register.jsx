35b7b950d82bebaaebcde381f1c14feef6731fb2
DevLop-EC <christlopbsc12@gmail.com>
2022-06-18 10:46:19 -0500

    Register page design

--- netflix-client/src/pages/Register.jsx
+++ netflix-client/src/pages/Register.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 import ApplicationLogo from '../components/ApplicationLogo';
 
-export const Login = () => {
+export const Register = () => {
   return (
     <div class="bg-no-repeat bg-cover bg-center relative bg__auth">
       <div class="absolute bg-gradient-to-b from-gray-900 to-black opacity-75 inset-0 z-0"></div>
@@ -13,39 +13,61 @@ export const Login = () => {
           <form class="p-8 backdrop-sepia-0 bg-black/60 mx-auto rounded-2xl w-100 ">
             <div class="mb-4 ">
               <h3 class="font-semibold text-2xl text-gray-200 flex justify-center items-center">
-                Inicia con tu cuenta
+                Registra una cuenta
               </h3>
               <p class="text-gray-500 text-sm flex justify-center items-center">
-                no tienes cuenta?
+                ya tienes una?
                 <Link
-                  to="/register"
+                  to="/"
                   class="text-sm p-1 text-red-500 hover:text-red-600"
                 >
-                  Registrate
+                  Inicia aqui
                 </Link>
               </p>
             </div>
-            <div class="space-y-5">
-              <div class="space-y-2">
+            <div class="space-y-3">
+              <div class="space-y-1">
+                <label class="text-sm font-medium text-gray-500 tracking-wide">
+                  Nombre de usuario
+                </label>
+                <input
+                  class=" w-full text-sm px-4 py-2 bg-slate-700 rounded-full text-gray-200"
+                  type="text"
+                  name="name"
+                  placeholder="Ingresa tu nombre de usuario"
+                />
+              </div>
+              <div class="space-y-1">
                 <label class="text-sm font-medium text-gray-500 tracking-wide">
                   Correo electronico
                 </label>
                 <input
-                  class=" w-full text-base px-4 py-2 bg-slate-700 rounded-full text-gray-200"
+                  class=" w-full text-sm px-4 py-2 bg-slate-700 rounded-full text-gray-200"
                   type="email"
                   name="email"
                   placeholder="mail@gmail.com"
                 />
               </div>
-              <div class="space-y-2">
+              <div class="space-y-1">
                 <label class="mb-2 text-sm font-medium text-gray-500 tracking-wide">
                   Contraseña
                 </label>
                 <input
-                  class="w-full content-center text-base px-4 py-2 bg-slate-700 rounded-full text-gray-200"
+                  class="w-full content-center text-sm px-4 py-2 bg-slate-700 rounded-full text-gray-200"
                   type="password"
                   name="password"
                   placeholder="Ingresa tu contraseña"
+                />
+              </div>
+              <div class="space-y-1">
+                <label class="mb-2 text-sm font-medium text-gray-500 tracking-wide">
+                  Confirmar contraseña
+                </label>
+                <input
+                  class="w-full content-center text-sm px-4 py-2 bg-slate-700 rounded-full text-gray-200"
+                  type="password"
+                  name="confirm_password"
+                  placeholder="Repite tu contraseña"
                 />
               </div>
               <div class="flex items-center justify-between">
@@ -54,19 +76,15 @@ export const Login = () => {
                     id="remember_me"
                     name="remember_me"
                     type="checkbox"
-                    class="h-4 w-4 bg-blue-500 focus:ring-blue-400 rounded"
+                    class="w-3 h-3 text-red-600 accent-red-500 rounded border-gray-300focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
                   />
+
                   <label
                     for="remember_me"
-                    class="ml-2 block text-sm text-gray-200"
+                    class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
                   >
-                    Recordarme
+                    Acepto los términos y condiciones
                   </label>
-                </div>
-                <div class="text-sm">
-                  <a href="#" class="text-red-500 hover:text-red-600">
-                    Contraseña olvidada?
-                  </a>
                 </div>
               </div>
               <div>
@@ -74,19 +92,8 @@ export const Login = () => {
                   type="submit"
                   class="w-full flex justify-center bg-red-500  hover:bg-red-600 text-gray-100 p-2  rounded-full tracking-wide font-semibold  shadow-lg cursor-pointer transition ease-in duration-500"
                 >
-                  Iniciar sesion
+                  Registrar
                 </button>
-                <span className="text-gray-200 flex justify-center items-center">
-                  o
-                </span>
-                <div class="flex  space-x-2 justify-center items-end bg-gray-200 hover:bg-gray-300 text-gray-600 py-2 rounded-full transition duration-100">
-                  <img
-                    class=" h-5 cursor-pointer"
-                    src="https://i.imgur.com/arC60SB.png"
-                    alt=""
-                  />
-                  <button>Inicia con Google</button>
-                </div>
               </div>
             </div>
           </form>

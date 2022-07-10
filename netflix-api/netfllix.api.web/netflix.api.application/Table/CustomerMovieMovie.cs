ccdd0a15eb711f372fc7662b32023ea7ba87b37d
DevLop-EC <christlopbsc12@gmail.com>
2022-07-10 14:08:22 -0500

    Metodos en controlador de autenticacion y auth service

---
+++ netflix-api/netfllix.api.web/netflix.api.application/Table/CustomerMovieMovie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace netflix.api.application.Table
+{
+    public partial class CustomerMovieMovie
+    {
+        public int CustomerMovieIdMovie { get; set; }
+        public int MovieMovieId { get; set; }
+
+        public virtual CustomerMovie CustomerMovieIdMovieNavigation { get; set; }
+        public virtual Movie MovieMovie { get; set; }
+    }
+}
